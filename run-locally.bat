e4918ca06641de56913c94b7f149c632f38f6d13
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2020-07-07 00:45:37 +0200

    Customize a few bits and add favicon

--- run-locally.bat
+++ run-locally.bat
@@ -1,3 +1,4 @@
-docker build -t local-eduherminio.github.io .
+docker build -t local-eduherminio.github.io --label blog-image .
+docker image prune --filter="label=blog-image" --force
 docker-compose up
 REM wsl docker run -p 8080:4000 -v $(pwd):/site --rm -it $(docker build -q .)  # Only works in wsl due to pwd
